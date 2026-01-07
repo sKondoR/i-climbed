977df3136fc3d4153dff41228ba0163c79dc146b
kondrse <kondor85@mail.ru>
2026-01-07 11:33:05 +0300

    add routes page

---
+++ src/app/routes/page.tsx
@@ -0,0 +1,12 @@
+import { PageDescription } from '@/app/ui/PageDescription';
+export default function RoutesPage() {
+  return (
+    <>
+      <div className="mt-3">
+        <PageDescription>
+          список трасс
+        </PageDescription>
+      </div>
+    </>
+  );
+}
