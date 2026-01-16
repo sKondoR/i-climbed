a06e0f16ad47dbbead6cce7c939f9823397ebfa4
kondrse <kondor85@mail.ru>
2026-01-16 17:24:39 +0300

    test build

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import NextFederationPlugin from '@module-federation/nextjs-mf';
+// import NextFederationPlugin from '@module-federation/nextjs-mf';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -9,49 +9,49 @@ const nextConfig: NextConfig = {
     'playwright',
     '@sparticuz/chromium',
   ],
-   webpack: (config, context) => {
-    const mfConfig = new NextFederationPlugin({
-      name: 'host_app',
-      filename: 'remoteEntry.js',
-      remotes: {
-        // microfrontend: 'microfrontend@http://localhost:3001/_next/static/chunks/remoteEntry.js',
-        // Продакшен (по необходимости):
-        microfrontend: `microfrontend@https://edit-route-image-mf.vercel.app/_next/static/chunks/remoteEntry.js?__t=${Date.now()}`,
-      },
-      shared: {
-        // react: {
-        //   singleton: true,
-        //   eager: true,
-        //   requiredVersion: false,
-        // },
-        // 'react-dom': {
-        //   singleton: true,
-        //   eager: true,
-        //   requiredVersion: false,
-        // },
-      },
-      extraOptions: {
-        // automaticAsyncBoundary: true,
-        // enableCompatibility: true,
-      },
-    });
+  //  webpack: (config, context) => {
+  //   const mfConfig = new NextFederationPlugin({
+  //     name: 'host_app',
+  //     filename: 'remoteEntry.js',
+  //     remotes: {
+  //       // microfrontend: 'microfrontend@http://localhost:3001/_next/static/chunks/remoteEntry.js',
+  //       // Продакшен (по необходимости):
+  //       microfrontend: `microfrontend@https://edit-route-image-mf.vercel.app/_next/static/chunks/remoteEntry.js?__t=${Date.now()}`,
+  //     },
+  //     shared: {
+  //       // react: {
+  //       //   singleton: true,
+  //       //   eager: true,
+  //       //   requiredVersion: false,
+  //       // },
+  //       // 'react-dom': {
+  //       //   singleton: true,
+  //       //   eager: true,
+  //       //   requiredVersion: false,
+  //       // },
+  //     },
+  //     extraOptions: {
+  //       // automaticAsyncBoundary: true,
+  //       // enableCompatibility: true,
+  //     },
+  //   });
 
-    config.plugins.push(mfConfig);
-    
-    // Keep externals
-    config.externals.push('playwright-core', 'playwright', '@sparticuz/chromium');
+  //   config.plugins.push(mfConfig);
+
+  //   // Keep externals
+  //   config.externals.push('playwright-core', 'playwright', '@sparticuz/chromium');
 
 
-    // Настройка publicPath только на клиенте
-    if (!context.isServer) {
-      config.output = {
-        ...config.output,
-        publicPath: 'auto',
-      };
-    }
+  //   // Настройка publicPath только на клиенте
+  //   if (!context.isServer) {
+  //     config.output = {
+  //       ...config.output,
+  //       publicPath: 'auto',
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 export default nextConfig;
