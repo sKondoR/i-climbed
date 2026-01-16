e29627d5a25fc0dc3f984be9358610a117081143
kondrse <kondor85@mail.ru>
2026-01-16 17:10:07 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,15 @@
 import NextFederationPlugin from '@module-federation/nextjs-mf';
-
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Явно отключаем webpack и разрешаем использование Turbopack,
-  // но с пустой конфигурацией, чтобы подавить ошибку
-  turbopack: {}, // ← это подавляет ошибку, но остаётся совместимым с webpack-плагинами в development
-
-  // ВАЖНО: Webpack-конфигурация НЕ будет использоваться при включённом Turbopack
-  // Module Federation пока НЕ поддерживается в Turbopack, поэтому принудительно используем webpack
-};
-
-// Экспортируем улучшенную конфигурацию с отключением Turbopack
-const config = {
-  ...nextConfig,
-  // Принудительно отключаем Turbopack, чтобы работал @module-federation/nextjs-mf
-  webpack: (config, context) => {
+  turbopack: {},
+  serverExternalPackages: [
+    'playwright-core',
+    'playwright',
+    '@sparticuz/chromium',
+  ],
+   webpack: (config, context) => {
     const mfConfig = new NextFederationPlugin({
       name: 'host_app',
       filename: 'remoteEntry.js',
@@ -63,8 +53,6 @@ const config = {
 
     return config;
   },
-} satisfies NextConfig;
+};
 
-// Экспортируем через withBundleAnalyzer
-// export default withBundleAnalyzer(config);
-export default config;
+export default nextConfig;
