005423e50af650f2becaea4496902b072de3e8ea
kondrse <kondor85@mail.ru>
2026-02-05 14:38:27 +0300

    fix

--- src/shared/ui/RouteBadge/RouteBadge.tsx
+++ src/shared/ui/RouteBadge/RouteBadge.tsx
@@ -10,7 +10,7 @@ const RouteBadge = ({
   const formattedGrade = grade.toLowerCase();
   const bg = GRADES_COLORS[formattedGrade.slice(0, 2)] || PROEKT_COLOR;
   return (
-    <span className="w-14 h-14 md:w-s17 md:h-17 rounded-full text-md md:text-xl inline-block text-center text-white bold pt-4 md:pt-5" style={{ background: bg }}>{formattedGrade}</span>
+    <span className="w-14 h-14 md:w-17 md:h-17 rounded-full text-md md:text-xl inline-block text-center text-white bold pt-4 md:pt-5" style={{ background: bg }}>{formattedGrade}</span>
   );
 };
 
